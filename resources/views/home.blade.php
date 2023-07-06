f50f29fa6c7296cc2165e7e3a7660dd883e9fa76
eduardonwa <eduardongua@hotmail.com>
2023-07-05 21:34:39 -0700

    ratio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,13 @@
 
     <div class="md:mx-auto md:col-start-2 md:col-end-5 lg:mx-0 row-start-1 lg:p-6">
         <div>
-            <article class="relative lg:h-0 md:p-0 pb-1/3 lg:p-1/3">
-                <img class="relative lg:absolute lg:inset-0 w-full lg:h-full lg:object-top object-cover" src="{{ $latestPost->getThumbnail() }}" alt="">
+            <article class="relative">
+                <div class="">
+                    <img class=""
+                        src="{{ $latestPost->getThumbnail() }}"
+                        alt="heavy metal latest post"
+                    >
+                </div>
                 <div class="absolute bottom-0 left-0 right-0 py-2 bg-gradient-to-t from-black to-transparent lg:p-16 p-3 lg:pb-5">
                     <h1 class="mb-3 uppercase font-bold bg-red-600 mx-1 p-1 w-max text-center text-white">
                         @foreach($latestPost->categories as $category)
