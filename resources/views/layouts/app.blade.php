79428eab0412f093bf404d2cb4515a12205a0049
eduardonwa <eduardongua@hotmail.com>
2023-07-05 17:35:48 -0700

    added a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,6 @@
                                 <x-slot name="trigger">
                                     <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 dark:text-white dark:bg-zinc-600 focus:outline-none transition ease-in-out duration-150">
                                         <div>{{ Auth::user()->name }}</div>
-
                                         <div class="ml-1">
                                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -114,7 +113,6 @@
                                     <x-dropdown-link :href="route('profile.edit')">
                                         {{ __('Profile') }}
                                     </x-dropdown-link>
-
                                     <!-- Authentication -->
                                     <form method="POST" action="{{ route('logout') }}">
                                         @csrf
@@ -143,12 +141,50 @@
         {{ $slot }}
     </div>
 
-    <footer class="w-full bg-gray-200 pb-12 dark:bg-zinc-900">
-        <div class="pt-2 text-center text-lg dark:text-gray-400">
-            Sick Of Metal
-            <p class="text-lg text-gray-700 dark:text-gray-500">
-                {{ \App\Models\TextWidget::getTitle('header') }}
-            </p>
+    <footer class="bg-gray-300 py-4 dark:bg-zinc-900">
+        <div class="py-1 flex items-center justify-center gap-x-4 w-full dark:text-gray-400">
+            <a href="https://instagram.com/sickofmetalnet">
+                <!-- Instagram -->
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 hover:text-gray-200 transition ease-in-out"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+            </svg>
+            </a>
+
+            <a href="https://youtube.com/@SickOfMetal">
+                <!-- Youtube -->
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 hover:text-gray-200 transition ease-in-out"
+                    fill="currentColor"
+                    viewBox="0 0 24 24">
+                <path
+                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+                </svg>
+            </a>
+        </div>
+
+        <div class="py-5 mx-auto container flex items-center justify-center space-x-5">
+            @foreach ($categories as $category)
+            <a
+                class="hover:text-gray-400 text-black dark:text-gray-400 dark:hover:text-gray-200 transition ease-in-out"
+                href="{{ route('by-category', ['category'=>$category['slug']]) }}">
+                {{ $category->title }}
+            </a>
+            @endforeach
+        </div>
+        <div class="mx-auto container flex items-center justify-center">
+            <a href="/">
+                <img
+                    class="w-64 py-4"
+                    src="{{ \App\Models\TextWidget::getImage('header') }}"
+                >
+            </a> <!-- logo end -->
         </div>
     </footer>
 
